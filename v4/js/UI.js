fcd89ee999ecfa4b9b3c5a41ebac983790f03965
nico <nico@urknall.co>
2019-06-11 17:10:57 +0200

    move app data into const object; new icon; dataset fixes; game title fix; minor css;

--- v4/js/UI.js
+++ v4/js/UI.js
@@ -7,55 +7,38 @@ class UI {
     this.question = document.getElementById('question')
   }
 
-  showLanguageMenu() {
-    this.clearUI()
-    this.languages.style.display = 'flex'
-  }
-
-  showGamesMenu() {
-    this.clearUI()
-    this.games.style.display = 'flex'
-  }
-
-  showLoader() {
-    this.clearUI()
-    this.loader.style.display = 'block'
-  }
-
   showGame() {
     this.clearUI()
-    if (app.data.gameMode.includes('flag-to')) this.flag.style.display = 'block'
-    else if (app.data.gameMode.includes('country-to')) this.country.style.display = 'block'
-    else if (app.data.gameMode.includes('capital-to')) this.capital.style.display = 'block'
+    if (DATA.gameMode.includes('flag-to')) this.flag.style.display = 'block'
+    else if (DATA.gameMode.includes('country-to')) this.country.style.display = 'block'
+    else if (DATA.gameMode.includes('capital-to')) this.capital.style.display = 'block'
     this.options.style.display = 'flex'
   }
 
   setQuestion(country) {
-    this.showGame()
     return new Promise((resolve, reject) => {
       this.flag.onload = () => resolve()
       this.flag.src = country.flag
-      this.country.textContent = country.name[app.data.language]
+      this.country.textContent = country.name[DATA.language]
       this.capital.textContent = country.capital
     })
   }
 
-  showOptions(options, solution) {
+  setOptions(options, solution) {
     this.options.innerHTML = ''
     options.forEach(option => {
       const btn = document.createElement('button')
       btn.className = 'button button-large button-raised button-fill'
       if (option.name === solution.name) btn.classList.add('solution')
-      if (app.data.gameMode.includes('to-country')) btn.textContent = option.name[app.data.language]
-      else if (app.data.gameMode.includes('to-capital')) btn.textContent = option.capital
-      else if (app.data.gameMode.includes('to-flag'))
-        btn.style.background = `center / cover no-repeat url(${option.flag})`
+      if (DATA.gameMode.includes('to-country')) btn.textContent = option.name[DATA.language]
+      else if (DATA.gameMode.includes('to-capital')) btn.textContent = option.capital
+      else if (DATA.gameMode.includes('to-flag')) btn.style.background = `center / cover no-repeat url(${option.flag})`
 
       this.options.appendChild(btn)
     })
   }
 
-  showSolution(clicked) {
+  revealSolution(clicked) {
     if (clicked.className !== 'solution') {
       clicked.classList.add('color-red')
       clicked.classList.add('border-color-red')
--- v4/js/app.js
+++ v4/js/app.js
@@ -1,5 +1,30 @@
 const $$ = Dom7
 
+const DATA = {
+  countries: [],
+  language: 'en',
+  languages: ['en', 'de', 'es', 'fr', 'it', 'nl', 'ja'],
+  region: 'world',
+  gameMode: 'flag-to-country',
+  gameModes: [
+    'flag-to-country',
+    'flag-to-capital',
+    'country-to-flag',
+    'country-to-capital',
+    'capital-to-flag',
+    'capital-to-country'
+  ],
+  optionsCount: {
+    'flag-to-country': 5,
+    'flag-to-capital': 5,
+    'country-to-flag': 3,
+    'country-to-capital': 5,
+    'capital-to-flag': 3,
+    'capital-to-country': 5
+  },
+  waitingTime: 1500
+}
+
 const app = new Framework7({
   root: '#app',
   name: 'What The Flag',
@@ -7,29 +32,6 @@ const app = new Framework7({
   version: '4.0.0',
   smartSelect: {
     closeOnSelect: true
-  },
-  data: {
-    countries: [],
-    language: 'en',
-    region: 'world',
-    gameMode: 'flag-to-country',
-    gameModes: [
-      'flag-to-country',
-      'flag-to-capital',
-      'country-to-flag',
-      'country-to-capital',
-      'capital-to-flag',
-      'capital-to-country'
-    ],
-    optionsCount: {
-      'flag-to-country': 5,
-      'flag-to-capital': 5,
-      'country-to-flag': 3,
-      'country-to-capital': 5,
-      'capital-to-flag': 3,
-      'capital-to-country': 5
-    },
-    waitingTime: 1000
   },
   routes: [
     {
@@ -50,36 +52,32 @@ const languageSelect = app.smartSelect.get('#languages')
 const gameSelect = app.smartSelect.get('#games')
 const regionSelect = app.smartSelect.get('#regions')
 
-loadSettings()
+// load app settings on app start
+$$(document).on('DOMContentLoaded', e => {
+  loadSettings()
+})
 
-// In page events:
+// init game when game page is loaded
 $$(document).on('page:init', function(e, page) {
   if (page.name === 'game') initGame()
 })
 
 $$('#play').on('click', () => {
-  app.data.language = languageSelect.getValue()
-  app.data.gameMode = gameSelect.getValue()
-  app.data.region = regionSelect.getValue()
+  DATA.language = languageSelect.getValue()
+  DATA.gameMode = gameSelect.getValue()
+  DATA.region = regionSelect.getValue()
 
   saveSettings()
 
-  const region = app.data.region === 'world' ? 'all' : 'region/' + app.data.region
+  const region = DATA.region === 'world' ? 'all' : 'region/' + DATA.region
   fetch(`https://restcountries.eu/rest/v2/${region}?fields=name;flag;capital;translations`)
     .then(res => res.json())
-    .then(countries => {
-      app.data.countries = countries.map(country => {
-        country.translations.en = country.name
-        country.name = country.translations
-        delete country.translations
-        return country
-      })
-    })
+    .then(countries => setCountryData(countries))
     .then(() => mainView.router.navigate('/game/'))
 })
 
 function initGame() {
-  const newTitle = app.data.gameMode
+  const newTitle = DATA.gameMode
     .split('-')
     .join(' ')
     .toUpperCase()
@@ -88,71 +86,95 @@ function initGame() {
   ui = new UI()
   ui.options.addEventListener('click', e => {
     if (e.target.nodeName !== 'BUTTON') return
-    ui.showSolution(e.target)
+    ui.revealSolution(e.target)
     clearTimeout(nextQuestionTimeout)
-    nextQuestionTimeout = setTimeout(nextQuestion, app.data.waitingTime)
+    nextQuestionTimeout = setTimeout(nextQuestion, DATA.waitingTime)
   })
 
   nextQuestion()
 }
 
 function nextQuestion() {
+  ui.clearUI()
+  app.preloader.show()
   let mixedMode
-  if (app.data.gameMode === 'mixed') {
+  if (DATA.gameMode === 'mixed') {
     mixedMode = true
-    app.data.gameMode = app.data.gameModes[Math.floor(Math.random() * app.data.gameModes.length)]
-    const newTitle = app.data.gameMode
+    DATA.gameMode = DATA.gameModes[Math.floor(Math.random() * DATA.gameModes.length)]
+    const newTitle = DATA.gameMode
       .split('-')
       .join(' ')
       .toUpperCase()
     $$('#game-title').text(newTitle)
   }
+
   const countries = getRandomCountries()
   const solutionIndex = Math.floor(Math.random() * countries.length)
   const solution = countries[solutionIndex]
 
   return ui
     .setQuestion(solution)
-    .then(() => ui.showOptions(countries, solution))
+    .then(() => ui.setOptions(countries, solution))
     .then(() => ui.showGame())
     .then(() => {
-      if (mixedMode) app.data.gameMode = 'mixed'
+      if (mixedMode) DATA.gameMode = 'mixed'
+      app.preloader.hide()
     })
 }
 
 function getRandomCountries() {
   const countries = []
   const indices = []
-  const count = app.data.optionsCount[app.data.gameMode]
+  const count = DATA.optionsCount[DATA.gameMode]
   let index
   for (let i = 0; i < count; i++) {
-    index = Math.floor(Math.random() * app.data.countries.length)
+    index = Math.floor(Math.random() * DATA.countries.length)
     if (!indices.includes(index)) {
       indices.push(index)
-      countries.push(app.data.countries[index])
+      countries.push(DATA.countries[index])
     } else i--
   }
   return countries
 }
 
+// load language, game mode and region from local storage
 function loadSettings() {
   const language = localStorage.getItem('language') || 'en'
   const gameMode = localStorage.getItem('gameMode') || 'mixed'
   const region = localStorage.getItem('region') || 'world'
 
-  console.log(language, gameMode, region)
-
-  app.data.language = language
-  app.data.gameMode = gameMode
-  app.data.region = region
+  DATA.language = language
+  DATA.gameMode = gameMode
+  DATA.region = region
 
   languageSelect.setValue(language)
   gameSelect.setValue(gameMode)
   regionSelect.setValue(region)
 }
 
+// save language, game mode and region to local storage
 function saveSettings() {
-  localStorage.setItem('language', app.data.language)
-  localStorage.setItem('gameMode', app.data.gameMode)
-  localStorage.setItem('region', app.data.region)
+  localStorage.setItem('language', DATA.language)
+  localStorage.setItem('gameMode', DATA.gameMode)
+  localStorage.setItem('region', DATA.region)
+}
+
+// process data coming from the api and set the country data for the app
+function setCountryData(countries) {
+  DATA.countries = countries.map(country => {
+    country.translations.en = country.name
+    country.name = country.translations
+    delete country.translations
+
+    // some countries do not have all translations, so I set them to the english name (11 is the normal amount of translations from the api)
+    if (Object.keys(country.name).length < 11) {
+      DATA.languages.forEach(language => {
+        if (!country.name[language]) country.name[language] = country.name.en
+      })
+    }
+
+    return country
+  })
+  // remove countries that have not all data (there are some that miss a capital)
+  DATA.countries = DATA.countries.filter(country => country.capital !== '')
 }
